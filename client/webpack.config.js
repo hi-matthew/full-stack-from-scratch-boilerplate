9d8f6c20c5927c54097bb2f48916cd12c8bb0256
Matthew Martinez <hellomattmartinez@gmail.com>
2019-03-14 16:31:08 -0700

    configure css loader + style loader in webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
             plugins: ["@babel/plugin-proposal-class-properties"]
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
